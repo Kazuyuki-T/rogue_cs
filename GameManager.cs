6a70b7498e52be8a8128e063eb698a65b0f3b6b6
Kazuyuki-T <s1610110@jaist.ac.jp>
2017-12-04 14:24:27 +0900

    rogue cs init

---
+++ GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rogue_cui_cs_git
+{
+    class GameManager
+    {
+        State state;
+        Player player;
+        Rule rule;
+
+        public GameManager() {
+
+        }
+
+        public void init() {
+            state = new State();
+            player = new Player();
+            rule = new Rule();
+        }
+
+        public void run() {
+
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rogue_cui_cs_git
+{
+    class Player
+    {
+        public Player() {
+
+        }
+    }
+}
